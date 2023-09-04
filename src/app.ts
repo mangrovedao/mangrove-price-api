35f45a941e3da563511e4b86bca9193706fc78e8
Louis-Amas <amaslouis@protonmail.com>
2023-09-04 09:40:38 +0200

    fix: support reversed pair

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,32 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import { AddressInfo } from 'net';
 import { Config } from './config/types';
-import { Context } from './types';
+import { Context, Price } from './types';
+
+export const reversePrice = (price: Price) => {
+  return {
+    open: 1 / price.open,
+    high: 1 / price.high,
+    low: 1 / price.low,
+    close: 1 / price.close,
+    volume: price.volume,
+  };
+};
+
+export const getPrice = (context: Context, pair: string, timeframe: string) => {
+  const prices = Object.keys(context.prices)
+    .map(exchange => {
+      if (!context.prices[exchange][timeframe] || !context.prices[exchange][timeframe][pair]) {
+        return undefined;
+      }
+
+      return context.prices[exchange][timeframe][pair];
+    })
+    .filter(value => value !== undefined)
+    .sort((a, b) => (b!.date.getTime() - a!.date.getTime())); // sort in descing order
+
+  return prices;
+}
 
 export const startApp = async (config: Config, context: Context) => {
   const app = express();
@@ -30,25 +55,21 @@ export const startApp = async (config: Config, context: Context) => {
 
   logger.info(`🚀 Server ready at ${url}`);
 
-
   app.get("/prices/:token0/:token1/:timeframe", (req, res) => {
-    const pair = `${req.params.token0}/${req.params.token1}`
-    const prices = Object.keys(context.prices)
-      .map(exchange => {
-        if (!context.prices[exchange][req.params.timeframe] || !context.prices[exchange][req.params.timeframe][pair]) {
-          return undefined;
-        }
-
-        return context.prices[exchange][req.params.timeframe][pair];
-      })
-      .filter(value => value !== undefined)
-      .sort((a, b) => (b!.date.getTime() - a!.date.getTime())); // sort in descing order
+    let pair = `${req.params.token0}/${req.params.token1}`
+    let prices = getPrice(context, pair, req.params.timeframe);
+    let reversed = false;
+    if (prices.length === 0)  {
+      pair = `${req.params.token1}/${req.params.token0}`;
+      prices = getPrice(context, pair, req.params.timeframe);
+      reversed = true;
+    }
 
     if (prices.length === 0) {
       return res.status(404).send();
     }
 
-    return res.status(200).json(prices[0]);
+    return res.status(200).json(reversed ? reversePrice(prices[0]!) : prices[0]);
   });
 
 
